1e4728d6a45d3d163da435f2731b430d7fe75800
Roman Koval <Romkka636@gmail.com>
2021-12-28 20:02:56 +0200

    2 способа реверса

--- app/src/main/java/com/example/task1/MainActivity.java
+++ app/src/main/java/com/example/task1/MainActivity.java
@@ -3,14 +3,40 @@ package com.example.task1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void onClickReverse(View view){
+
+        //String ReverseString = "";
+        //TextView textView = findViewById(R.id.textview);
+        //EditText edittext =  findViewById(R.id.edittext);
+        //String userString=edittext.getText().toString();
+        //char[] EnterString = userString.toCharArray();
+        //for(int count=EnterString.length-1;count>=0;count--){
+       //     ReverseString = ReverseString + EnterString[count];
+        //}
+        //textView.setText(ReverseString);
+
+        EditText edittext =  findViewById(R.id.edittext);
+        TextView textView = findViewById(R.id.textview);
+        String userString=edittext.getText().toString();
+        StringBuffer strBuffer = new StringBuffer(userString);
+        strBuffer.reverse();
+        textView.setText(strBuffer);
+
 
 
 
